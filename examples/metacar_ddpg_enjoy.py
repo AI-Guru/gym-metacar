74cb06317dfdf77d418a3c69394e9e9a6961a067
Tristan AI-Guru <tristan@ai-guru.de>
2019-06-10 22:11:53 +0200

    Add enjoy script for DDPG.

---
+++ examples/metacar_ddpg_enjoy.py
@@ -0,0 +1,37 @@
+import gym
+try:
+    import gym_metacar
+except:
+    import sys
+    sys.path.append(".")
+    sys.path.append("..")
+    import gym_metacar
+
+from gym_metacar.wrappers import *
+
+from stable_baselines import DDPG
+from stable_baselines.common.vec_env import *
+
+env_id = "metacar-random-continuous-v0"
+env = gym.make(env_id)
+env.enable_webrenderer()
+env = LinearObservationWrapper(env)
+env = TerminateWrapper(env)
+env = ClipRewardsWrapper(env)
+env = DummyVecEnv([lambda:env])
+env = VecFrameStack(env, n_stack=4)
+
+# Load the trained agent
+model = DDPG.load("metacar-ddpg")
+
+# Enjoy trained agent
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+    if done == True:
+        env.reset()
+        continue
+    env.render()
+
+env.close()
